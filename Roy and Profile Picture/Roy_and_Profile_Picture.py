86c44b127e64a8383a607c373b7d5747d60d105e
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-05 23:06:44 -0400

    Add project files.

---
+++ Roy and Profile Picture/Roy_and_Profile_Picture.py
@@ -0,0 +1,17 @@
+﻿# قراءة قيمة L
+L = int(input())
+
+# قراءة عدد الصور N
+N = int(input())
+
+# معالجة كل صورة
+for _ in range(N):
+    W, H = map(int, input().split())
+    
+    if W < L or H < L:
+        print("UPLOAD ANOTHER")
+    elif W == H:
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
+
